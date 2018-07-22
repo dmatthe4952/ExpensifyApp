f91d6f89a50db85b2a893c9430ed82ab0cdf93cb
david <dmatthe@yahoo.com>
2018-07-21 20:55:15 -0500

    refactor filestructure and create devdependencies section

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,8 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js',
-            publicPath: "/public/"
+            path: path.join(__dirname, 'public','dist'),
+            filename: 'bundle.js'
         },
         module: {
             rules: [
@@ -46,7 +45,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: "/dist/"
         },
         plugins: [
             CSSExtract
